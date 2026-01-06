cf618ee7e1d11dbc2a91f49e79734d47ba7c352f
avaprohaska-1234 <avaprohaska@gmail.com>
2026-01-06 00:00:02 +0000

    created default vpc, ec2 error

--- main.tf
+++ main.tf
@@ -12,3 +12,17 @@ provider "aws" {
   region = var.region
 }
 
+# Create a VPC
+resource "aws_vpc" "vpc_tf" {
+  cidr_block = "10.0.0.0/16"
+}
+
+#add ec2 instance
+resource "aws_instance" "ec2_tf" {
+  ami           = var.ami_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "ec2_tf"
+  }
+}
